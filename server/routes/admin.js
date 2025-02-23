55aa4b52f34fabf2e75c455c9407c6a4b97421c1
Daniel <danielhakobyan460@gmail.com>
2025-02-23 19:45:37 +0400

    Fixed issue1

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -78,24 +78,24 @@ router.post("/admin/login", async (req, res) => {
 // });
 
 //Post Admin-Register
-// router.post("/admin/register", async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     const hashedPassword = await bcrypt.hash(password, 10);
+router.post("/admin/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-//     try {
-//       const user = await User.create({ username, password: hashedPassword });
-//       res.status(201).json({ message: "User created successfully", user });
-//     } catch (error) {
-//       if (error.code === 11000) {
-//         res.status(409).json({ message: "User already in use" });
-//       }
-//       res.status(500).json({ message: "Internal server error" });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "User created successfully", user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: "User already in use" });
+      }
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // Get / Admin Dashboard
 
